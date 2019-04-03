b5f0f1884c705b4c4143e5a76116167da5d1613b
Niclas <niclas.kroon@protonmail.ch>
2019-04-03 20:06:50 +0200

    retrieves image successfully from local storage

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,23 @@
 
 use std::fs::File;
 
+//  Get a url
+//  Check if its in cache, on disk if it is we serve that
+//  otherwise we fetch from SMHI
+//  colorize it via libtiff and convert to png
+//  return a png file
+
+
 #[macro_use] extern crate rocket;
 use rocket::{get, routes};
 use rocket::response::content;
+use rocket::http::ContentType;
 
 #[get("/version/latest/area/sweden/product/comp/<year>/<month>/<day>/<filename>")]
-fn retrieve(year: String, month: String, day: String, filename: String) -> Option<content::Plain<File>> {
+fn retrieve(year: String, month: String, day: String, filename: String) -> Option<content::Content<File>> {
     let file = format!("{year}/{month}/{day}/{filename}", year = year, month = month, day = day, filename = filename);
     println!("{}", file);
-    File::open(&file).map(|f| content::Plain(f)).ok()
+    File::open(&file).map(|f| content::Content(ContentType::PNG, f)).ok()
 }
 
 #[catch(404)]
