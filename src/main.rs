2edc2eefc848e79d6a290f374f8bea3d46331577
Niclas <niclas.kroon@protonmail.ch>
2019-04-05 13:45:27 +0200

    outputs random colored image now

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use std::io::Read;
 use std::path::Path;
 use std::time::{Duration, Instant};
 
+use rand::Rng;
+
 use image::DynamicImage;
 use image::GenericImageView;
 use image::ImageBuffer;
@@ -31,125 +33,122 @@ fn mkdir(path: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-//  (((54 + 54) + 54) * 0.4) + (-30) gets the dBZ gain
+fn get_rand_col(alpha: u8) -> Rgba<u8> {
+    let mut rng = rand::thread_rng();
+    let r = rng.gen_range(1, 255);
+    let g = rng.gen_range(1, 255);
+    let b = rng.gen_range(1, 255);
+    //println!("Color: R: {}, G: {}, B: {}, A: {}", r, g, b, alpha);
+    Rgba([r, g, b, alpha])
+}
 
-fn gain(rgb: u8) -> Rgba([u8]) {
-    // calculate GAIN dBZ values
-    // gain = 0.4
-    // offset = -30.0
-    // dBZ = pixel_value * gain * offset
-    let arr: [[u8; 3]; 100] = [
-        [54, 54, 54],
-        [55, 55, 55],
-        [56, 56, 56],
-        [57, 57, 57],
-        [58, 58, 58],
-        [59, 59, 59],
-        [60, 60, 60],
-        [61, 61, 61],
-        [62, 62, 62],
-        [63, 63, 63],
-        [64, 64, 64],
-        [65, 65, 65],
-        [66, 66, 66],
-        [67, 67, 67],
-        [68, 68, 68],
-        [69, 69, 69],
-        [70, 70, 70],
-        [71, 71, 71],
-        [72, 72, 72],
-        [73, 73, 73],
-        [74, 74, 74],
-        [75, 75, 75],
-        [76, 76, 76],
-        [87, 87, 87],
-        [97, 97, 97],
-        [108, 108, 108],
-        [118, 118, 118],
-        [129, 129, 129],
-        [139, 139, 139],
-        [150, 150, 150],
-        [160, 160, 160],
-        [171, 171, 171],
-        [181, 181, 181],
-        [192, 192, 192],
-        [0, 50, 255],
-        [0, 70, 255],
-        [0, 90, 255],
-        [0, 110, 255],
-        [0, 130, 255],
-        [0, 150, 255],
-        [0, 170, 255],
-        [0, 128, 0],
-        [0, 138, 0],
-        [0, 148, 0],
-        [0, 158, 0],
-        [0, 163, 0],
-        [0, 168, 0],
-        [0, 173, 0],
-        [0, 178, 0],
-        [10, 208, 10],
-        [10, 218, 10],
-        [10, 228, 10],
-        [10, 238, 10],
-        [10, 248, 10],
-        [255, 255, 15],
-        [255, 246, 15],
-        [255, 238, 15],
-        [255, 229, 15],
-        [255, 220, 15],
-        [255, 200, 0],
-        [255, 180, 0],
-        [255, 160, 0],
-        [255, 140, 0],
-        [255, 120, 0],
-        [255, 35, 35],
-        [255, 15, 15],
-        [255, 0, 0],
-        [235, 0, 0],
-        [215, 0, 0],
-        [195, 0, 0],
-        [175, 0, 0],
-        [155, 0, 0],
-        [135, 0, 0],
-        [115, 0, 0],
-        [175, 0, 175],
-        [184, 0, 184],
-        [193, 0, 193],
-        [202, 0, 202],
-        [211, 0, 211],
-        [219, 0, 219],
-        [228, 0, 228],
-        [237, 0, 237],
-        [246, 0, 246],
-        [255, 0, 255],
-        [0, 255, 255],
-        [13, 255, 255],
-        [26, 255, 255],
-        [39, 255, 255],
-        [51, 255, 255],
-        [64, 255, 255],
-        [77, 255, 255],
-        [90, 255, 255],
-        [102, 255, 255],
-        [115, 255, 255],
-        [128, 255, 255],
-        [141, 255, 255],
-        [154, 255, 255],
-        [166, 255, 255],
-        [179, 255, 255],
-        [192, 255, 255],
-    ];
+fn gain(rgb: [u8; 3], alpha: u8) -> Rgba<u8> {
+    /*let pixel_rgb: [u8; 3] = [192,255,255];*/
 
-    for x in &arr[..] {
-        println!(
-            "x,y: RGB: {:?}, R: {:?}, G: {:?}, B: {:?},",
-            x, x[0], x[1], x[2]
-        )
-    }
-
-    if [[r][g][b]] == array[r][g][b] {
-        println!("Lol: {:?}", array);
+    match rgb {
+        [0, 0, 0] => Rgba([0,0,0, 255/4]),
+        [54, 54, 54] => get_rand_col(alpha),
+        [55, 55, 55] => get_rand_col(alpha),
+        [56, 56, 56] => get_rand_col(alpha),
+        [57, 57, 57] => get_rand_col(alpha),
+        [58, 58, 58] => get_rand_col(alpha),
+        [59, 59, 59] => get_rand_col(alpha),
+        [60, 60, 60] => get_rand_col(alpha),
+        [61, 61, 61] => get_rand_col(alpha),
+        [62, 62, 62] => get_rand_col(alpha),
+        [63, 63, 63] => get_rand_col(alpha),
+        [64, 64, 64] => get_rand_col(alpha),
+        [65, 65, 65] => get_rand_col(alpha),
+        [66, 66, 66] => get_rand_col(alpha),
+        [67, 67, 67] => get_rand_col(alpha),
+        [68, 68, 68] => get_rand_col(alpha),
+        [69, 69, 69] => get_rand_col(alpha),
+        [70, 70, 70] => get_rand_col(alpha),
+        [71, 71, 71] => get_rand_col(alpha),
+        [72, 72, 72] => get_rand_col(alpha),
+        [73, 73, 73] => get_rand_col(alpha),
+        [74, 74, 74] => get_rand_col(alpha),
+        [75, 75, 75] => get_rand_col(alpha),
+        [76, 76, 76] => get_rand_col(alpha),
+        [87, 87, 87] => get_rand_col(alpha),
+        [97, 97, 97] => get_rand_col(alpha),
+        [108, 108, 108] => get_rand_col(alpha),
+        [118, 118, 118] => get_rand_col(alpha),
+        [129, 129, 129] => get_rand_col(alpha),
+        [139, 139, 139] => get_rand_col(alpha),
+        [150, 150, 150] => get_rand_col(alpha),
+        [160, 160, 160] => get_rand_col(alpha),
+        [171, 171, 171] => get_rand_col(alpha),
+        [181, 181, 181] => get_rand_col(alpha),
+        [192, 192, 192] => get_rand_col(alpha),
+        [0, 50, 255] => get_rand_col(alpha),
+        [0, 70, 255] => get_rand_col(alpha),
+        [0, 90, 255] => get_rand_col(alpha),
+        [0, 110, 255] => get_rand_col(alpha),
+        [0, 130, 255] => get_rand_col(alpha),
+        [0, 150, 255] => get_rand_col(alpha),
+        [0, 170, 255] => get_rand_col(alpha),
+        [0, 128, 0] => get_rand_col(alpha),
+        [0, 138, 0] => get_rand_col(alpha),
+        [0, 148, 0] => get_rand_col(alpha),
+        [0, 158, 0] => get_rand_col(alpha),
+        [0, 163, 0] => get_rand_col(alpha),
+        [0, 168, 0] => get_rand_col(alpha),
+        [0, 173, 0] => get_rand_col(alpha),
+        [0, 178, 0] => get_rand_col(alpha),
+        [10, 208, 10] => get_rand_col(alpha),
+        [10, 218, 10] => get_rand_col(alpha),
+        [10, 228, 10] => get_rand_col(alpha),
+        [10, 238, 10] => get_rand_col(alpha),
+        [10, 248, 10] => get_rand_col(alpha),
+        [255, 255, 15] => get_rand_col(alpha),
+        [255, 246, 15] => get_rand_col(alpha),
+        [255, 238, 15] => get_rand_col(alpha),
+        [255, 229, 15] => get_rand_col(alpha),
+        [255, 220, 15] => get_rand_col(alpha),
+        [255, 200, 0] => get_rand_col(alpha),
+        [255, 180, 0] => get_rand_col(alpha),
+        [255, 160, 0] => get_rand_col(alpha),
+        [255, 140, 0] => get_rand_col(alpha),
+        [255, 120, 0] => get_rand_col(alpha),
+        [255, 35, 35] => get_rand_col(alpha),
+        [255, 15, 15] => get_rand_col(alpha),
+        [255, 0, 0] => get_rand_col(alpha),
+        [235, 0, 0] => get_rand_col(alpha),
+        [215, 0, 0] => get_rand_col(alpha),
+        [195, 0, 0] => get_rand_col(alpha),
+        [175, 0, 0] => get_rand_col(alpha),
+        [155, 0, 0] => get_rand_col(alpha),
+        [135, 0, 0] => get_rand_col(alpha),
+        [115, 0, 0] => get_rand_col(alpha),
+        [175, 0, 175] => get_rand_col(alpha),
+        [184, 0, 184] => get_rand_col(alpha),
+        [193, 0, 193] => get_rand_col(alpha),
+        [202, 0, 202] => get_rand_col(alpha),
+        [211, 0, 211] => get_rand_col(alpha),
+        [219, 0, 219] => get_rand_col(alpha),
+        [228, 0, 228] => get_rand_col(alpha),
+        [237, 0, 237] => get_rand_col(alpha),
+        [246, 0, 246] => get_rand_col(alpha),
+        [255, 0, 255] => get_rand_col(alpha),
+        [0, 255, 255] => get_rand_col(alpha),
+        [13, 255, 255] => get_rand_col(alpha),
+        [26, 255, 255] => get_rand_col(alpha),
+        [39, 255, 255] => get_rand_col(alpha),
+        [51, 255, 255] => get_rand_col(alpha),
+        [64, 255, 255] => get_rand_col(alpha),
+        [77, 255, 255] => get_rand_col(alpha),
+        [90, 255, 255] => get_rand_col(alpha),
+        [102, 255, 255] => get_rand_col(alpha),
+        [115, 255, 255] => get_rand_col(alpha),
+        [128, 255, 255] => get_rand_col(alpha),
+        [141, 255, 255] => get_rand_col(alpha),
+        [154, 255, 255] => get_rand_col(alpha),
+        [166, 255, 255] => get_rand_col(alpha),
+        [179, 255, 255] => get_rand_col(alpha),
+        [192, 255, 255] => get_rand_col(alpha),
+        [255, 255, 255] => Rgba([255, 255, 255, 0]),
+        _ => get_rand_col(alpha),
     }
 }
 
@@ -212,23 +211,23 @@ fn retrieve(year: String, month: String, day: String, filename: String) -> Optio
                 // TODO just summarize all rgb values.. match against
                 // our new color scheme. maybe a color ini file?
 
-                let new_pixel = image::Rgba([r, g, b, a]);
-
+                //let new_pixel = image::Rgba([r, g, b, a]);
+                let new_pixel = gain([r,g,b], a);
 
                 new_colorized_image.put_pixel(x, y, new_pixel)
             }
 
-            new_colorized_image.save("nybild.png").unwrap();
+            new_colorized_image.save(new_path).unwrap();
 
             // THIS IS HOW WE SHOULD SAVE BUT NE
-            img.save(new_path).unwrap();
+            //img.save(new_path).unwrap();
         }
         None => {
             panic!("Error");
         }
     }
 
-    let new_path = Path::new("2019/04/03/radar_1904030005.png");
+    let new_path = Path::new("2008/10/01/radar_0810010000.png");
 
     println!("Elapsed: {:?}", start.elapsed());
     let f = File::open(&new_path).map(|f| content::Content(ContentType::PNG, f)).ok();
